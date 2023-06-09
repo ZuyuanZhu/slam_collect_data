7597acd6642771b723c64b08135af5c5fa4ee895
Zuyuan Zhu <zuyuanzhu@gmail.com>
2023-06-09 10:38:35 +0100

    update sequence number for each message's frame

--- src/data_collect.py
+++ src/data_collect.py
@@ -12,6 +12,9 @@ class DataCollector:
         rospy.init_node('data_collector', anonymous=True)
         self.bag = rosbag.Bag('jackal_zed.bag', 'w')
 
+        # Counter for sequence numbers
+        self.seq = 0
+
         # Subscriptions
         sub_img = message_filters.Subscriber("/zed/rgb/image_raw_color", Image)
         sub_imu = message_filters.Subscriber("/zed/imu/data", Imu)
@@ -19,10 +22,22 @@ class DataCollector:
         sub_gps = message_filters.Subscriber("/jackal_gps/fix", NavSatFix)
 
         # ApproximateTimeSynchronizer to synchronize Image, Imu, Odometry and GPS data
+        # start with a lower slop value (e.g., 0.1 or even 0.05 seconds) and see if that works
+        # for the application, and then adjust as necessary based on the observed results.
         ts = message_filters.ApproximateTimeSynchronizer([sub_img, sub_imu, sub_odom, sub_gps], queue_size=10, slop=0.5)
         ts.registerCallback(self.callback)
 
     def callback(self, img, imu, odom, gps):
+        # Update sequence number for each message's frame
+        frame_id = str(self.seq).zfill(10)
+        img.header.frame_id = frame_id
+        imu.header.frame_id = frame_id
+        odom.header.frame_id = frame_id
+        gps.header.frame_id = frame_id
+
+        # Increase the sequence number
+        self.seq += 1
+
         self.bag.write('/zed/rgb/image_raw_color', img)
         self.bag.write('/zed/imu/data', imu)
         self.bag.write('/jackal_velocity_controller/odom', odom)
@@ -36,4 +51,3 @@ if __name__ == '__main__':
         print("Shutting down")
     finally:
         dc.bag.close()
-
